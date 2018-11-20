d6a1669d6efb9bc295d9e6b35103d8780b455358
Gabriel Bibiano <gabriel.bibiano.a@gmail.com>
2018-11-20 15:57:00 -0200

    First version

---
+++ NavigatorShare.vue
@@ -0,0 +1,50 @@
+<template>
+  <div @click="share()">
+    <slot v-if="hasSlot()" name="clickable">
+
+    </slot>
+    <p v-else>Share</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: String,
+      text: String,
+      url: String,
+      onError: Function,
+      onSuccess: Function,
+    },
+    methods: {
+      hasSlot() {
+        return !!this.$slots['clickable']
+      },
+      share() {
+        const data = {
+          title: this.title,
+          text: this.text,
+          url: this.url
+        };
+
+        if (navigator.share) {
+          navigator.share(data)
+            .then((succ) => {
+              if (succ) {
+                this.onSuccess(succ);
+              }
+            })
+            .catch((err) => {
+              if (onError) {
+                this.onError(err);
+              }
+            })
+        } else {
+          if (this.onError) {
+            this.onError();
+          }
+        }
+      }
+    }
+  }
+</script>
